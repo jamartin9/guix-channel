3f8611eefa68b154864f3e8e824750756078ce22
Justin Martin <jaming@protonmail.com>
2021-05-06 08:54:41 -0400

    nvda graft browsers

---
+++ jam/packages/browsers.scm
@@ -0,0 +1,24 @@
+(define-module (jam packages browsers)
+  #:use-module (nongnu packages mozilla)
+  #:use-module (nongnu packages nvidia)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages chromium)
+  )
+
+(define ff-nvda
+  (package
+   (inherit firefox)
+   (name "firefox-nvda")))
+
+(define chrome-nvda
+  (package
+   (inherit ungoogled-chromium)
+   (name "ungoogled-chromium-nvda")))
+
+(define-public firefox-nvda
+  (replace-mesa ff-nvda))
+
+(define-public chromeium-nvda
+  (replace-mesa chrome-nvda))
