e80b953cb9384e57df35d2214095ebca24673951
Justin Martin <jaming@protonmail.com>
2022-01-21 11:44:04 -0500

    add xmrig and p2pool
    Signed-off-by: Justin Martin <jaming@protonmail.com>

---
+++ jam/packages/p2pool.scm
@@ -0,0 +1,127 @@
+(define-module (jam packages p2pool)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix build utils)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages pkg-config) ; pkg-config
+  #:use-module (gnu packages libevent) ; libuv
+  #:use-module (gnu packages gsasl) ; gss - p2pool
+  #:use-module (gnu packages networking) ; zeromq - p2pool
+  #:use-module (gnu packages crypto) ; libsodium - p2pool
+  #:use-module (guix build-system cmake) ; cmake
+  #:use-module (gnu packages gnunet) ; libmicrohttpd - xmrig
+  #:use-module (gnu packages mpi) ; hwloc - xmrig
+  #:use-module (gnu packages tls) ; openssl - xmrig
+  #:use-module ((gnu packages) #:prefix gnu:)
+  #:use-module (guix diagnostics)
+  #:use-module (guix i18n)
+  #:use-module (ice-9 regex)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-26)
+  )
+
+
+
+(define (search-patch file-name)
+  "Search the patch FILE-NAME.  Raise an error if not found."
+  (or (search-path (%patch-path) file-name)
+      (raise (formatted-message (G_ "~a: patch not found")
+                                file-name))))
+
+(define-syntax-rule (search-patches file-name ...)
+  "Return the list of absolute file names corresponding to each FILE-NAME found in %PATCH-PATH."
+  (list (search-patch file-name) ...))
+
+(define %channel-root
+  (find (lambda (path)
+          (file-exists? (string-append path "/jam/packages/p2pool.scm")))
+        %load-path))
+
+(define %patch-path
+  (make-parameter
+   (append
+    (list (string-append %channel-root "/jam/packages/patches"))
+    (gnu:%patch-path))))
+
+
+
+(define-public monero-p2pool
+  (let ((commit "248fcee1746d14e9e9ff8d0415c34bde924c8fb9")
+        (revision "1"))
+    (package
+      (name "p2pool-xmr")
+      (version (git-version "1.5" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (recursive? #t)
+                      (url "https://github.com/SChernykh/p2pool/")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1m3i1hmybczxbgvfz2kqb81bspk3nq114b3mxvm060632nngamrv"))
+                (modules '((guix build utils)))))
+      (build-system cmake-build-system)
+      (outputs '("out"))
+      (arguments
+       `(#:tests? #false
+         #:phases (modify-phases %standard-phases
+                                 (add-after 'install 'copy
+                                            (lambda* (#:key inputs outputs #:allow-other-keys)
+                                              (let* ((out (assoc-ref outputs "out"))
+                                                     (source (assoc-ref inputs "source")))
+                                              (mkdir-p (string-append out "/bin"))
+                                              (copy-file "p2pool" (string-append out "/bin" "/p2pool"))))))))
+      (inputs
+       (list libuv gss zeromq libsodium))
+      (native-inputs
+       (list pkg-config))
+      (home-page "https://p2pool.io/")
+      (synopsis "p2p mining xmr pool")
+      (description
+       "Decentralized pool for Monero mining.")
+      (license license:gpl3))))
+
+
+(define-public xmrig
+  (let ((commit "718c7e0fc1452353bc1361b85c8a2a4c81c1379e")
+        (revision "1"))
+    (package
+      (name "xmrig")
+      (version (git-version "6.16.2" revision commit))
+      (source (origin
+                (patches (search-patches "xmrig-donate.patch"))
+                (method git-fetch)
+                (uri (git-reference
+                      (recursive? #t)
+                      (url "https://github.com/xmrig/xmrig/")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1wf4r1wwbyjdrin5a7x1rwnivwcybf5h6gnyisdh3hyiz086zfj5"))
+                (modules '((guix build utils)))))
+      (build-system cmake-build-system)
+      (outputs '("out"))
+      (arguments
+       `(#:tests? #false
+         #:phases (modify-phases %standard-phases
+                                 (delete 'install)
+                                 (add-after 'build 'copy
+                                            (lambda* (#:key inputs outputs #:allow-other-keys)
+                                              (let* ((out (assoc-ref outputs "out"))
+                                                     (source (assoc-ref inputs "source")))
+                                              (mkdir-p (string-append out "/bin"))
+                                              (copy-file "xmrig" (string-append out "/bin" "/xmrig"))))))))
+      (inputs
+       (list libuv openssl `(,hwloc "lib") libmicrohttpd))
+      (native-inputs
+       (list pkg-config))
+      (home-page "https://xmrig.com/")
+      (synopsis "crypto miner")
+      (description
+       "Crypto mining for RandomX, KawPow, CryptoNight and AstroBTW.")
+      (license license:gpl3))))
