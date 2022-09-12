aa4033cff61d3892e6a706e25ec8e7bd2e16c4fa
Justin Martin <jaming@protonmail.com>
2022-09-12 14:47:33 -0400

    remove libgccjit for upstream
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/gcc.scm
+++ jam/packages/gcc.scm
@@ -11,57 +11,6 @@
   #:use-module (gnu packages commencement)
   )
 
-
-(define-public libgccjit-for-gcc
-  (mlambda (gcc)
-    (package
-      (inherit gcc)
-      (name "libgccjit")
-      (outputs (delete "lib" (package-outputs gcc)))
-      (properties (alist-delete 'hidden? (package-properties gcc)))
-      (arguments
-       (substitute-keyword-arguments `(#:modules ((guix build gnu-build-system)
-                                                  (guix build utils)
-                                                  (ice-9 regex)
-                                                  (srfi srfi-1)
-                                                  (srfi srfi-26))
-                                       ,@(package-arguments gcc))
-         ((#:configure-flags flags)
-          `(append '("--disable-bootstrap"
-                     "--disable-libatomic"
-                     "--disable-libgomp"
-                     "--disable-libquadmath"
-                     "--disable-libssp"
-                     "--enable-host-shared"
-                     "--enable-checking=release"
-                     "--enable-languages=jit")
-                   (remove (cut string-match "--enable-languages.*" <>)
-                           ,flags)))
-         ((#:phases phases)
-          `(modify-phases ,phases
-             (add-after 'install 'remove-broken-or-conflicting-files
-               (lambda* (#:key outputs #:allow-other-keys)
-                 (for-each delete-file
-                           (find-files (string-append (assoc-ref outputs "out") "/bin")
-                                       ".*(c\\+\\+|cpp|g\\+\\+|gcov|gcc|gcc-.*)"))
-                 #t))))))
-      (inputs
-       (alist-delete "libstdc++"
-                     (package-inputs gcc)))
-      (native-inputs
-       `(("gcc" ,gcc)
-         ,@(package-native-inputs gcc))))))
-
-(define-public libgccjit-12
-  (libgccjit-for-gcc gcc-12))
-
-(define-public libgccjit-11
-  (libgccjit-for-gcc gcc-11))
-
-(define-public libgccjit-10
-  (libgccjit-for-gcc gcc-10))
-
-
 ;;  "enable flags in modified glibc for PIE without nscd, and with static NSS modules "
 (define-public glibc-pie
   (package
