886840813f1a4c1d8bdf60995b20335f9788da85
Justin Martin <jaming@protonmail.com>
2024-03-18 14:21:35 -0400

    update nonguix/dape and remove emacs-code-review
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/emacs-xyz.scm
+++ jam/packages/emacs-xyz.scm
@@ -7,39 +7,16 @@
   #:use-module (jam packages)
   #:use-module ((guix licenses) #:prefix license:))
 
-(define-public emacs-code-review
-  (package
-    (name "emacs-code-review")
-    (version "20221209.1215")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/wandersoncferreira/code-review/")
-             (commit "26f426e99221a1f9356aabf874513e9105b68140")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1031sq40kysbkvl0cl4lq39ls13n0y3kafbmf4c30grbydljbd52"))
-       (patches (search-patches "emacs-code-review-closql-uuidgen.patch"))))
-    (build-system emacs-build-system)
-    (propagated-inputs
-     (list emacs-closql emacs-magit emacs-transient emacs-a emacs-ghub emacs-deferred emacs-markdown-mode emacs-forge emacs-emojify))
-    (home-page "https://github.com/wandersoncferreira/code-review/")
-    (synopsis "Package to help you perform code reviews from your VC provider.")
-    (description "Currently supports Github and basic Gitlab and Bitbucket workflows.")
-    (license license:gpl3+)))
-
-
 (define-public emacs-dape
   (package
    (name "emacs-dape")
-   (version "0.4.0")
+   (version "0.9.0")
    (source (origin
             (method url-fetch)
             (uri (string-append "https://elpa.gnu.org/packages/dape-" version
                                 ".tar"))
             (sha256 (base32
-                     "1yfv7mgr9jdw3i3a684fjvcb18asa92iy41f8xpn4qi36i1dbvyg"))))
+                     "13xvl24l8lr64ndc8rk1dxdgsbvwxhn90844xkdzl7qs8i3hyw1l"))))
    (build-system emacs-build-system)
    (home-page "https://github.com/svaante/dape")
    (synopsis "Debug Adapter Protocol for Emacs")
