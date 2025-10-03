2c063ff56a72a49019b110555ad6d3d287fa8d1f
Justin Martin <jaming@protonmail.com>
2025-10-03 10:52:09 -0400

    unbundle nzbget and par2-turbo
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/nzbget.scm
+++ jam/packages/nzbget.scm
@@ -30,24 +30,16 @@
     (arguments
      (append (list #:configure-flags '(list "-DBUILD_LIB=ON" "-DBUILD_TOOL=ON"))
      (list #:phases #~(modify-phases %standard-phases
-                               (delete 'check); skip tests
-                               (add-after 'unpack 'pre-install-source ; copy source
-                                          (lambda* (#:key outputs #:allow-other-keys)
-                                            (copy-recursively
-                                             "."
-                                             (string-append (assoc-ref outputs "src") "/src"))))
-                               (add-after 'install 'really-install
+                                     (delete 'check); skip tests
+                               (add-after 'install 'really-install ;(add-after 'install 'fail (lambda* (#:key outputs #:allow-other-keys) (exit 1)))
                                           (lambda* (#:key outputs #:allow-other-keys)
                                             (let ((out (assoc-ref outputs "out")))
                                               (install-file "par2" (string-append out "/bin"))
                                               (install-file "libpar2-turbo.a" (string-append out "/lib"))
                                               (install-file "libgf16.a" (string-append out "/lib"))
-                                              (install-file "libhasher.a" (string-append out "/lib")))))
-                               )
-           )
-     ))
-    (outputs '("out" "src"))
-    ;(native-inputs (list automake autoconf))
+                                              (install-file "libhasher.a" (string-append out "/lib"))
+                                              (copy-recursively "../source/include" (string-append (assoc-ref outputs "out") "/include")))))))))
+    (outputs '("out"))
     (build-system cmake-build-system)
     (synopsis "File verification and repair tools")
     (description "This is a simple fork of par2cmdline which replaces core computation routines with ParPar’s processing backend, improving par2cmdline’s performance on x86/ARM platforms.")
@@ -67,35 +59,20 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1182xfxsljh5n4z54snv68kgca8ica2fq8jcgd8yjg3amkwz7i3r"))
-       (patches (search-patches "nzbget.patch")) ; patch SOURCE_DIR to PAR2_ROOT unlike nixos
-       ))
+        (base32 "1182xfxsljh5n4z54snv68kgca8ica2fq8jcgd8yjg3amkwz7i3r"))))
    (arguments
-    (append ;(list #:configure-flags `(list (string-append "-DCMAKE_LIBRARY_PATH=" (assoc-ref %build-inputs "par2cmdline-turbo") "/lib")))
+    (append (list #:configure-flags `(list "-DCMAKE_CXX_FLAGS=-DPARPAR_ENABLE_HASHER_MD5CRC -DHAVE_CONFIG_H" "-DCMAKE_C_FLAGS=-DPARPAR_ENABLE_HASHER_MD5CRC -DHAVE_CONFIG_H")); cmake/par2-turbo.cmake: add_compile_definitions(HAVE_CONFIG_H PARPAR_ENABLE_HASHER_MD5CRC)
+
             (list #:tests? #f) ; skip tests due to errors
              (list #:phases
                    #~(modify-phases %standard-phases
                                     (add-after 'unpack 'sub-tests
                                                (lambda* (#:key inputs #:allow-other-keys)
-                                                 (let ((par-root (assoc-ref inputs "par2cmdline-turbo:src"))
-                                                       (par-lib (assoc-ref inputs "par2cmdline-turbo"))
-                                                       )
-                                                   (copy-recursively (string-append par-root "/src") "./par2-turbo-src") ; copy external projects src for includes
-                                                   (substitute* "cmake/par2-turbo.cmake"
-                                                                (("set\\(PAR2_ROOT.*") "set\(PAR2_ROOT ${CMAKE_SOURCE_DIR}/par2-turbo-src)\n") ; set root to copied src
-                                                                (("set\\(INCLUDES.*") "set\(INCLUDES ${INCLUDES} ${CMAKE_SOURCE_DIR}/par2-turbo-src/include)"); set include to copied src
-                                                                ;(("PREFIX.*par2-turbo") (string-append "PREFIX " (getcwd) "/../build/par2-turbo\n" ))
-                                                                ;(("set\\(LIBS.*") (string-append "set(LIBS ${LIBS} " par-lib "/lib/libpar2-turbo.a " par-lib "/lib/gf16.a " par-lib "/lib/hasher.a)\n"))
-                                                                ;(("ExternalProject_add\\(") "message(\n")
-                                                                (("set\\(LIBS.*") "find_library(par2-turbo libpar2-turbo.a gf16.a hasher.a)\ntarget_link_libraries(${PACKAGE} PRIVATE libpar2-turbo.a gf16.a hasher.a )\n"); TODO unbundle ExternalProject_add
-                                                                ))
-                                                 ))))
-             ))
-   (inputs `(("par2cmdline-turbo" ,par2cmdline-turbo) ; lib output?
-             ("par2cmdline-turbo:src" ,par2cmdline-turbo "src") ;; add src output for include headers
-             ,@(package-inputs nzbget)))
-   )
-  )
+                                                   (substitute* "cmake/posix.cmake"
+                                                                (("include\\(\\$\\{CMAKE_SOURCE_DIR}/cmake/par2-turbo.cmake)") "set(LIBS ${LIBS} libpar2-turbo.a gf16.a hasher.a)\n")
+                                                                (("set\\(DEPENDENCIES.*par2-turbo)") "\n"))))))))
+   (inputs `(("par2cmdline-turbo" ,par2cmdline-turbo)
+             ,@(package-inputs nzbget)))))
 
 
 ;par2cmdline-turbo
