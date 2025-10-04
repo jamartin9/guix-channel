1468a75aa7dbee793d6df6dc3e115f338c0b6528
Justin Martin <jaming@protonmail.com>
2025-10-03 22:48:40 -0400

    refactor pyzfs
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/zfs.scm
+++ jam/packages/zfs.scm
@@ -694,7 +694,11 @@
 ;
 ;are encouraged to hack on Guix to implement the above features.
 (define-module (jam packages zfs)
+   #:use-module (gnu packages)
    #:use-module (gnu packages file-systems)
+   #:use-module (gnu packages linux)
+   #:use-module (gnu packages python)
+   #:use-module (gnu packages python-xyz)
    #:use-module (gnu services)
    #:use-module (gnu services base)
    #:use-module (gnu services linux)
@@ -703,8 +707,10 @@
    #:use-module (gnu system mapped-devices)
    #:use-module (guix gexp)
    #:use-module (guix modules)
-   #:use-module (guix packages)
    #:use-module (guix records)
+   #:use-module (guix packages)
+   #:use-module (guix platform)
+   #:use-module (guix utils)
    #:use-module (srfi srfi-1)
    #:export (zfs-service-type
              zfs-configuration
@@ -1002,7 +1008,7 @@
 (define zfs-service-type
   (service-type
     (name 'zfs)
-    (extensions
+    (extensions ; TODO add pam module
       (list ;; Install OpenZFS kernel module into kernel profile.
             (service-extension linux-loadable-module-service-type
                                zfs-loadable-modules)
@@ -1029,3 +1035,36 @@
             (service-extension udev-service-type
                                (compose list make-zfs-package))))
     (description "Installs ZFS, an advanced filesystem and volume manager.")))
+
+(define-public pyzfs
+  (package
+   (inherit zfs)
+   (inputs (modify-inputs (package-inputs zfs)
+                          (append linux-pam)))
+   (native-inputs (modify-inputs (package-native-inputs zfs)
+                                 (append python-distlib)))
+   (arguments
+    (substitute-keyword-arguments (package-arguments zfs)
+                                  ((#:phases phases)
+                                   (let ((python-version (version-major+minor (package-version python))))
+                                     #~(modify-phases #$phases
+                                                      (add-after 'install 'pyinstall ; copy libzfs_core python lib
+                                                                 (lambda* (#:key inputs native-inputs make-flags parallel-build? #:allow-other-keys)
+                                                                   (copy-recursively "contrib/pyzfs/build/lib" (string-append #$output "/lib/python" #$python-version "/site-packages"))))
+                                                      (replace 'really-configure
+                                                               (lambda* (#:key inputs outputs #:allow-other-keys)
+                                                                 (let* ((out (assoc-ref outputs "out")))
+                                                                   (substitute* "configure" (("-/bin/sh") (string-append "-" (which "sh"))))
+                                                                   (invoke "./configure"
+                                                                           "--with-config=all"
+                                                                           "--enable-pyzfs"; add python bindings
+                                                                           "--enable-pam"; add pam module for mounting datasets on ssh login
+                                                                           (string-append "--with-pammoduledir=" out "/lib/security/"); pam
+                                                                           (string-append "--with-pamconfigsdir=" out "/etc/pam.d/"); pam
+                                                                           (string-append "--prefix=" out)
+                                                                           (string-append "--with-dracutdir=" out "/lib/dracut")
+                                                                           (string-append "--with-udevdir=" out "/lib/udev")
+                                                                           (string-append "--with-mounthelperdir=" out "/sbin")
+                                                                           (string-append "--with-linux=" (search-input-directory inputs "lib/modules/build")))))))))))))
+
+pyzfs
