f4fda0a2f034a5aa275b0635e79d23dcbe658057
Justin Martin <jaming@protonmail.com>
2025-10-11 21:44:47 -0400

    bump nzbget and enable tests
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/nzbget.scm
+++ jam/packages/nzbget.scm
@@ -30,7 +30,7 @@
     (arguments
      (append (list #:configure-flags '(list "-DBUILD_LIB=ON" "-DBUILD_TOOL=ON"))
              (list #:phases #~(modify-phases %standard-phases
-                                             (delete 'check); skip tests despite make check and WITH_TESTS
+                                             (delete 'check); skip tests despite make check and WITH_TESTS. MAYBE add native inputs for replacing ctest with make check
                                              (add-after 'install 'really-install ;(add-after 'install 'fail (lambda* (#:key outputs #:allow-other-keys) (exit 1)))
                                                         (lambda* (#:key outputs #:allow-other-keys)
                                                           (let ((out (assoc-ref outputs "out")))
@@ -50,7 +50,7 @@
   (package
    (inherit nzbget)
    (name "nzbget-next")
-   (version "25.3")
+   (version "25.4")
    (source
      (origin
        (method git-fetch)
@@ -59,21 +59,21 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1182xfxsljh5n4z54snv68kgca8ica2fq8jcgd8yjg3amkwz7i3r"))))
+        (base32 "06ysbn9fykjhd8v9qdc0y6gfpbd1adkmpm1vw5b5lb8304va9z84"))))
    (arguments
-    (append (list #:configure-flags `(list "-DCMAKE_CXX_FLAGS=-DPARPAR_ENABLE_HASHER_MD5CRC -DHAVE_CONFIG_H" "-DCMAKE_C_FLAGS=-DPARPAR_ENABLE_HASHER_MD5CRC -DHAVE_CONFIG_H")); cmake/par2-turbo.cmake: add_compile_definitions(HAVE_CONFIG_H PARPAR_ENABLE_HASHER_MD5CRC)
+    (append (list #:configure-flags `(list "-DCMAKE_CXX_FLAGS=-DPARPAR_ENABLE_HASHER_MD5CRC -DHAVE_CONFIG_H" "-DENABLE_TESTS=1")); cmake/par2-turbo.cmake: add_compile_definitions(HAVE_CONFIG_H PARPAR_ENABLE_HASHER_MD5CRC)
 
-            (list #:tests? #f) ; skip tests due to errors
             (list #:phases
                   #~(modify-phases %standard-phases
-                                   (add-after 'unpack 'sub-tests
-                                              (lambda* (#:key inputs #:allow-other-keys)
+                                   (add-after 'unpack 'unbundle-par2
+                                              (lambda _
                                                 (substitute* "cmake/posix.cmake"
                                                              (("include\\(\\$\\{CMAKE_SOURCE_DIR}/cmake/par2-turbo.cmake)") "set(LIBS ${LIBS} libpar2-turbo.a gf16.a hasher.a)\n")
                                                              (("set\\(DEPENDENCIES.*par2-turbo)") "\n"))))))))
    (inputs `(("par2cmdline-turbo" ,par2cmdline-turbo)
-             ,@(package-inputs nzbget)))))
+             ,@(package-inputs nzbget)))
+   (home-page "https://github.com/nzbgetcom/nzbget")))
 
 
-;par2cmdline-turbo
-nzbget-next
+par2cmdline-turbo
+;nzbget-next
