8106ae96f6854335c469f19935c9cde5853c5bb3
Justin Martin <jaming@protonmail.com>
2026-03-16 22:30:17 -0400

    bump ik_llama
    Signed-off-by: Justin Martin <jaming@protonmail.com>

--- jam/packages/llama.scm
+++ jam/packages/llama.scm
@@ -13,16 +13,16 @@
   (package
    (inherit llama-cpp)
    (name "ik-llama-cpp")
-   (version "git-714329f4ca633a2962d3813210a73384e416bd0a") ; add commit to version to avoid false base32 hash passing on reinstall
+   (version "git-54bcafe") ; add commit to version to avoid false base32 hash passing on reinstall
    (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/ikawrakow/ik_llama.cpp")
-             (commit "714329f4ca633a2962d3813210a73384e416bd0a")))
+             (commit "54bcafee1682847ea8884319e752803ea8ff17d7")))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1ksrahn981f9dpvv5svmgs89ak31jbdsj4fnllcki53063mq28gr"))))
+        (base32 "00ylj8nzhdl6f6avzbbq7afnlshy5ma80wxzdif3haj1v66wxw0x"))))
    (arguments
     (substitute-keyword-arguments (package-arguments llama-cpp)
                                   ((#:configure-flags flags '("-DGGML_IQK_FA_ALL_QUANTS=ON" "-DGGML_NATIVE=ON")) ''("-DGGML_IQK_FA_ALL_QUANTS=ON" "-DGGML_NATIVE=ON"))
